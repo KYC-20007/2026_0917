7a27a64125801516f85cffed0125317c80f7971c
ash-0921 <D1546836@o365.fcu.edu.tw>
2026-09-17 11:17:09 +0800

    2

--- D1546836.py
+++ D1546836.py
@@ -1,6 +1,6 @@
-for i in range(1,5):
-    print("*****")
-    print("*****")
-    print("*****")
-    print("*****")
-    print("*****")
+
+print("*****")
+print("*****")
+print("*****")
+print("*****")
+print("*****")
